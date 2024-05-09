068c2d2d0dd674ec00d049678241287e465d70c3
tiamiyu00 <tiamiyuolamideahmed@gmail.com>
2024-05-09 12:59:50 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Project from "./component/Project";
+
+
+function App() {
+  return ( <div>
+
+    <Header/>
+
+    <Project/>
+
+    <Footer/>
+    
+    </div>
+  );
+}
+export default App;
